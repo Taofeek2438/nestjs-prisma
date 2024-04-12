476fe911f873d6afabc7c7fe6091c4083880e71a
Adekola Taofeek <85371080+Taofeek2438@users.noreply.github.com>
2024-04-12 01:28:45 +0100

    config

---
+++ src/enum/base.enum.ts
@@ -0,0 +1,14 @@
+export enum TokenEnum {
+  email = 'email',
+  password = 'password'
+}
+
+export enum UserRole {
+  talent = 'talent',
+  creative = 'creative'
+}
+
+export enum SortUser {
+  date = "date",
+  name = "name"
+}
